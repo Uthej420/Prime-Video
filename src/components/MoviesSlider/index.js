47adcbbcf0742ef373cae5922b96377668ccd9d6
Uthej420 <uthejpalnati99@gmail.com>
2023-01-19 05:51:12 +0000

    prime video app added

---
+++ src/components/MoviesSlider/index.js
@@ -0,0 +1,27 @@
+import Slider from 'react-slick'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import MovieItem from '../MovieItem'
+
+const MoviesSlider = props => {
+  const settings = {
+    dots: false,
+    slidesToScroll: 1,
+    slidesToShow: 4,
+  }
+  const {moviesList} = props
+
+  return (
+    <>
+      <Slider {...settings}>
+        {moviesList.map(eachMovie => (
+          <MovieItem movieDetails={eachMovie} key={eachMovie.id} />
+        ))}
+      </Slider>
+    </>
+  )
+}
+
+export default MoviesSlider
